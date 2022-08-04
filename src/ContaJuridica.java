8712b76197a7e19ff4c5eb56cc1f0324d85f7dcb
regi1297 <reginald_miranda@hotmail.com>
2022-08-03 21:31:47 -0300

    Criação do banco digital

---
+++ src/ContaJuridica.java
@@ -0,0 +1,15 @@
+import java.lang.constant.Constable;
+
+public class ContaJuridica extends Conta {
+
+     public ContaJuridica(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Juridica ===");
+        super.imprimirInfosComuns();
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente Reginaldo = new Cliente();
+        Reginaldo.setNome("Reginaldo");
+        Cliente Regiane = new Cliente();
+        Regiane.setNome("Regiane");
+
+        Conta cc = new ContaCorrente(Reginaldo);
+        Conta poupanca = new ContaPoupanca(Reginaldo);
+        Conta juridica = new  ContaJuridica(Regiane);
+
+        cc.depositar(200);
+        cc.transferir(100, poupanca);
+        juridica.depositar(100);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+        juridica.imprimirExtrato();
+    }
+
+}
